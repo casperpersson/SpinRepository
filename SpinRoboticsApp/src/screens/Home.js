72a222bd0c675b8435573247b38eff6bc5b9ef4f
Jonas Grubbe <grubbe.p@hotmail.com>
2024-04-16 14:19:19 +0200

    Casper's changes

---
+++ SpinRoboticsApp/src/screens/Home.js
@@ -0,0 +1,126 @@
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  */
+
+// import React from 'react';
+// import type {PropsWithChildren} from 'react';
+// import {
+//   SafeAreaView,
+//   ScrollView,
+//   StatusBar,
+//   StyleSheet,
+//   Text,
+//   useColorScheme,
+//   View,
+//   Image,
+  
+// } from 'react-native';
+
+// import {
+//   Colors,
+// } from 'react-native/Libraries/NewAppScreen';
+
+// type SectionProps = PropsWithChildren<{
+//   title: string;
+// }>;
+
+// function Section({children, title}: SectionProps): React.JSX.Element {
+//   const isDarkMode = useColorScheme() === 'dark';
+//   return (
+//     <View style={styles.sectionContainer}>
+//       <Text
+//         style={[
+//           styles.sectionTitle,
+//           {
+//             color: isDarkMode ? Colors.white : Colors.black,
+//           },
+//         ]}>
+//         {title}
+//       </Text>
+//       <Text
+//         style={[
+//           styles.sectionDescription,
+//           {
+//             color: isDarkMode ? Colors.light : Colors.dark,
+//           },
+//         ]}>
+//         {children}
+//       </Text>
+//     </View>
+//   );
+// }
+
+
+
+// function App(): React.JSX.Element {
+//   const isDarkMode = useColorScheme() === 'dark';
+
+//   const backgroundStyle = {
+//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+//   };
+
+//   return (
+//     <SafeAreaView style={backgroundStyle}>
+//     <StatusBar
+//       barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+//       backgroundColor={backgroundStyle.backgroundColor}
+//     />
+//     <ScrollView
+//       contentInsetAdjustmentBehavior="automatic"
+//       style={backgroundStyle}>
+      
+//       <View
+//         style={{
+//           backgroundColor: isDarkMode ? Colors.black : Colors.white,
+//         }}>
+          
+//           <Image
+//         style={styles.Logo}
+//         source={require('./Screens/spin-roboticsweb.jpg')}
+//       />
+//       <Text style={styles.Big}>Velkommen til spin robitics nye App!!😎😎</Text>
+//       </View>
+//     </ScrollView>
+//   </SafeAreaView>
+ 
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   Big: {
+//     color: 'black',
+//     fontWeight: 'bold',
+//     fontSize: 34,
+//     textAlign: 'center' 
+//   },
+//   Logo: {
+//     width: 400,
+//     height: 200
+    
+//     },
+
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+//   container: {
+//     paddingTop: 50,
+//   },
+// });
+
+// export default App;
