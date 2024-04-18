726ebf0c73ce55009457cdf4157c29271acf38fb
casperpersson <persson1201@gmail.com>
2024-04-18 10:13:24 +0200

    xdd

--- SpinRoboticsApp/App.tsx
+++ SpinRoboticsApp/App.tsx
@@ -9,77 +9,16 @@ import {
   Text,
   useColorScheme,
   View,
-  Image,
-  
 } from 'react-native';
-
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-    <StatusBar
-      barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-      backgroundColor={backgroundStyle.backgroundColor}
-    />
-    <ScrollView
-      contentInsetAdjustmentBehavior="automatic"
-      style={backgroundStyle}>
-      
-      <View
-        style={{
-          backgroundColor: isDarkMode ? Colors.black : Colors.white,
-        }}>
-          
-          <Image
-        style={styles.Logo}
-        source={require('./Screens/spin-roboticsweb.jpg')}
-      />
-      <Text style={styles.Big}>Velkommen til spin robitics nye App!!😎😎</Text>
-      </View>
-    </ScrollView>
-  </SafeAreaView>
  
+import AppNavigator from './src/app.navigator';
+
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+      <AppNavigator></AppNavigator>
   );
 }
 
@@ -88,18 +27,6 @@ const styles = StyleSheet.create({
     marginTop: 32,
     paddingHorizontal: 24,
   },
-  Big: {
-    color: 'black',
-    fontWeight: 'bold',
-    fontSize: 34,
-    textAlign: 'center' 
-  },
-  Logo: {
-    width: 400,
-    height: 200
-    
-    },
-
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
@@ -111,9 +38,6 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
-  },
-  container: {
-    paddingTop: 50,
   },
 });
 
--- SpinRoboticsApp/src/screens/Results.js
+++ SpinRoboticsApp/src/screens/Results.js
@@ -1,12 +1,68 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, Alert, Image } from 'react-native';
+import { Table, TableWrapper, Row, Cell } from 'react-native-table-component';
 
-const Results = () => {
-    return(
-        <View>
-            <Text>Results</Text>
+
+export default class ExampleFour extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableHead: ['Time', 'Program Name', 'Evaluation', 'Final Torque','Final insertion depth', 'Final Angle', 'detail'],
+      tableData: [
+        ['10:04:08\n22/02/2024', 'Den her', 'OK', '1.20Nm', '10.80mm', '10', '0'],
+        ['10:04:08\n22/02/2024', 'Den her', 'OK', '1.20Nm', '10.80mm', '10', '0'],
+        ['10:04:08\n22/02/2024', 'Den her', 'OK', '1.20Nm', '10.80mm', '10', '0'],
+        ['10:04:08\n22/02/2024', 'Den her', 'OK', '1.20Nm', '10.80mm', '10', '0'],
+      ]
+    }
+  }
+
+  _alertIndex(index) {
+    Alert.alert(`Her er der nogle detaljer eller noget`);
+  }
+
+  render() {
+    const state = this.state;
+    const element = (data, index) => (
+      <TouchableOpacity onPress={() => this._alertIndex(index)}>
+        <View style={styles.btn}>
+          <Text style={styles.btnText}>button</Text>
         </View>
-    
-    )};
+      </TouchableOpacity>
+    );
 
-    export default Results;
+    return (
+      <View style={styles.container}>
+        <Table borderStyle={{borderColor: 'black', borderWidth: 1}}>
+          <Row data={state.tableHead} style={styles.head} textStyle={styles.text}/>
+          {
+            state.tableData.map((rowData, index) => (
+              <TableWrapper key={index} style={styles.row}>
+                {
+                  rowData.map((cellData, cellIndex) => (
+                    <Cell key={cellIndex} data={cellIndex === 6 ? element(cellData, index) : cellData} textStyle={styles.text}/>
+                  ))
+                }
+              </TableWrapper>
+            ))
+          }
+        </Table>
+        <Image
+           style={styles.Logo}
+           source={require('../assets/spin-roboticsweb.jpg')}
+         />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: '#fff' },
+  head: { height: 50, backgroundColor: 'White' },
+  text: { margin: 6 },
+  row: { flexDirection: 'row', backgroundColor: 'lightgrey'},
+  btn: { margin: 5, width: 50, height: 18, backgroundColor: 'lightblue',  borderRadius: 2 },
+  btnText: { textAlign: 'center', color: '#fff' },
+  Logo: { width: 400, height: 200}
+    
+});
